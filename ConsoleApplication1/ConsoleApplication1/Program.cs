c35d3407f6247bba29d0e309f0850a5e3459538a
bulin.niu <bulin.niu@dfjy-jx.com>
2023-05-18 22:34:50 +0800

    Init

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sort = new int[] { 2, 4, 12, 343, 45, 2, 545, 65767 };
+            QuickSort(sort, 0, sort.Length);
+        }
+
+        public static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+                QuickSort(arr, left, pivot - 1);
+                QuickSort(arr, pivot + 1, right);
+            }
+        }
+
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int i = left - 1;
+            for (int j = left; j < right; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            int temp2 = arr[i + 1];
+            arr[i + 1] = arr[right];
+            arr[right] = temp2;
+            return i + 1;
+        }
+    }
+}
